d660663bb95d219a9444b86bcebb7dab5487b991
Rezis Github <cosmonshop@gmail.com>
2026-02-25 13:03:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
 CHAT_ID = os.environ.get("CHAT_ID", "")
-ANTHROPIC_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
 MODE = os.environ.get("MODE", "morning")  # "morning" or "evening"
